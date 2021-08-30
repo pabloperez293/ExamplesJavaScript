7dd0a348300a9ca885230d4f95981bde39f03f16
pabloperez293 <56325243+pabloperez293@users.noreply.github.com>
2021-08-30 15:54:39 -0300

    Add files via upload

---
+++ Ciclos-For-while/main.js
@@ -0,0 +1,17 @@
+// ciclo while
+let dias = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+
+var numeroDias = dias.length -1 ;
+let i = 0;
+
+while (i <= numeroDias){
+    document.write( dias[i] + "<br>");
+    i++;
+}
+
+// Ciclo for
+// let dias = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+
+// for( i = 0; i <= dias.length - 1; i++){
+//     document.write(dias[i] +"<br>");
+// }
---
+++ condiciones/main.js
@@ -0,0 +1,16 @@
+let name = "jamie";
+let year =  28;
+
+
+if ( year == 18){
+    document.write("Bienvenido");
+}else {
+    document.write("No sos mayor de edad");
+}
+// if (name == "papablopo"){
+//     document.write("Bienvenido "+ name);
+// } else if (name == "jaime"){
+
+// }else {
+
+// }
